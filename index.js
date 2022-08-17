082fb67814649d74ec0ff49fd25ffc5161435a58
gupta82k11 <89532606+gupta82k11@users.noreply.github.com>
2022-08-17 22:58:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+
+function musicPlay1(){
+    const music1 = new Audio('Valam - Made In China 320 Kbps.mp3');
+    music1.play();
+}
+function musicPlay2(){
+    const music2 = new Audio('Nain.mp3');
+    music2.play();
+}
+function musicPlay3(){
+    const music3 = new Audio('Nazar Na Lag Jaaye (Stree) 128 Kbps.mp3');
+    music3.play();
+}
+function musicPlay4(){
+    const music4 = new Audio('Tu Hi Mera - Jannat 2 320Kbps.mp3');
+    music4.play();
+}
+function musicPlay5(){
+    const music5 = new Audio('03. Criminal.mp3');
+    music5.play();
+}
+
+function musicPlay6(){
+    const music6 = new Audio('03. Pehli Nazar Mein.mp3');
+    music6.play();
+}
+function musicPlay7(){
+    const music7 = new Audio('Aao Raja (Yo Yo Honey Singh) Gabbar Is Back.mp3');
+    music7.play();
+}
+function musicPlay8(){
+    const music8 = new Audio('01 Chogada - Loveratri.mp3.crdownload');
+    music8.play();
+}
+function musicPlay9(){
+    const music9 = new Audio('Sawan Mein Lag Gayi Aag - Badshah.mp3');
+    music9.play();
+}
+function musicPlay10(){
+    const music10 = new Audio('Mere Sohneya - Kabir Singh.mp3');
+    music10.play();
+}
+function musicPlay11(){
+    const music11 = new Audio('Hui Malang - Malang.mp3');
+    music11.play();
+}
+function musicPlay12(){
+    const music12 = new Audio('Backbone - Harrdy Sandhu.mp3');
+    music12.play();
+}
+function musicPlay13(){
+    const music13 = new Audio('Nachunga Aise - Millind Gaba.mp3');
+    music13.play();
+}
+function musicPlay14(){
+    const music14 = new Audio('Jug Jug Jeeve - Shiddat.mp3');
+    music14.play();
+}
+function musicPlay15(){
+    const music15 = new Audio('Mummy Nu Pasand - Jai Mummy Di 128 Kbps.mp3');
+    music15.play();
+}
+function musicPlay16(){
+    const music16 = new Audio('Kamli.mp3');
+    music16.play();
+}
+function musicPlay17(){
+    const music17 = new Audio('');
+    music17.play();
+}
+function musicPlay18(){
+    const music17 = new Audio('Man Bharya 2.mp3');
+    music17.play();
+}
+function musicPlay19(){
+    const music17 = new Audio('Kaun Nachdi - Sonu Ke Titu Ki Sweety.mp3.crdownload');
+    music17.play();
+} 
+function musicPlay20(){
+    const music17 = new Audio('Humnava Mere - Jubin Nautiyal.mp3');
+    music17.play();
+}
+
+
+
+
+
